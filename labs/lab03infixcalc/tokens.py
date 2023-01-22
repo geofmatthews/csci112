65d85068ef130b96247fac81a494519ceeb10ffb
Geoffrey Matthews <gmatthews@wlu.edu>
2023-01-22 13:20:48 -0500

    lab 3 notes

--- labs/lab03infixcalc/tokens.py
+++ labs/lab03infixcalc/tokens.py
@@ -1,18 +1,61 @@
-import string
+'''
+Tokenizes infix arithmetric expressions.
+converts ints and floats
+symbols: + = * / **
+and parentheses left as strings
 
-def parse_token(s):
-    if len(s) > 1:
-        if s[0] in string.digits + '+-':
-            return parse_number(s)
-    return s
+Geoffrey Matthews
+2023
+'''
+
+import string
 
 def parse_number(s):
     try:
         n = int(s)
     except:
-        n = float(s)
+        try:
+            n = float(s)
+        except:
+            return s
     return n
+
+def split_string(s):
+    if len(s) == 0:
+        return []
+    elif s[0] in string.whitespace:
+        return split_string(s[1:])
+    elif s[0] in '()+/':
+        return [s[0]] + split_string(s[1:])
+    elif s[0] == '*':
+        if s[1] == '*':
+            return [s[0:2]] + split_string(s[2:])
+        else:
+            return [s[0]] + split_string(s[1:])
+    elif s[0] == '-':
+        if s[1] not in string.digits:
+            return [s[0]] + split_string(s[1:])
+    toke = ''
+    if s[0] in '+-':
+        toke += s[0]
+        s = s[1:]
+    while s and s[0] in string.digits:
+        toke += s[0]
+        s = s[1:]
+    if s and s[0] in '.':
+        toke += s[0]
+        s = s[1:]
+    while s and s[0] in string.digits:
+        toke += s[0]
+        s = s[1:]
+    if s and s[0] in 'e':
+        toke += s[0]
+        s = s[1:]
+    while s and s[0] in string.digits:
+        toke += s[0]
+        s = s[1:]
+    return [toke] + split_string(s)
     
 def tokenize(s):
-    return [parse_token(s1) for s1 in s.split()]
+    return [parse_number(s1) for s1 in split_string(s)]
 
--- labs/lab03infixcalc/tokens_test.py
+++ labs/lab03infixcalc/tokens_test.py
@@ -3,10 +3,17 @@ from tokens import tokenize
 
 class test_tokenize(unittest.TestCase):
     def test_tokens(self):
-        self.assertEqual([123], tokenize('123'))
-        self.assertEqual([123, '+', 321], tokenize('123 + 321'))
-        self.assertEqual(['(', 123, '+', -321, ')'], tokenize('( 123 + -321 )'))
+        self.assertEqual([1e10], tokenize('1e10'))
+        self.assertEqual([-123], tokenize('-123'))
+        self.assertEqual([0.5], tokenize('.5'))
+        self.assertEqual([123, '+', 321], tokenize('123  + 321'))
+        self.assertEqual(['(', 123, '+', -321, ')'], tokenize('(123+-321)'))
         self.assertEqual([1e2], tokenize('1e2'))
+        self.assertEqual([], tokenize(''))
+        self.assertEqual([3], tokenize('3'))
+        self.assertEqual([3, '-', -2], tokenize('3--2'))
+        self.assertEqual([33, '**', 44], tokenize('33**44'))
+                         
 
 if __name__ == '__main__':
     unittest.main()
