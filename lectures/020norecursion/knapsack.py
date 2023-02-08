20ba81046bc1b11fdfbce2abbcc25eff367b0c41
Matthews <gmatthews@wlu.edu>
2023-02-08 10:56:55 -0500

    knapsack

--- lectures/020norecursion/knapsack.py
+++ lectures/020norecursion/knapsack.py
@@ -19,29 +19,27 @@ def knapsack_recursive(itemlist, totalweight):
         return max(val_with_item, val_without_item)
 
 def knapsack_dynamic(itemlist, totalweight):
-    tab = dict()
+    values = dict()
     for item in range(len(itemlist)+1):
         for weight in range(totalweight+1):
             if item == 0 or weight == 0:
-                tab[item,weight] = 0
+                values[item,weight] = 0
             elif itemlist[item-1].weight <= weight:
                 a = itemlist[item-1].value + \
-                    tab[item-1, weight-itemlist[item-1].weight]
-                b = tab[item-1, weight]
-                tab[item,weight] = max(a,b)
+                    values[item-1, weight-itemlist[item-1].weight]
+                b = values[item-1, weight]
+                values[item,weight] = max(a,b)
             else:
-                tab[item,weight] = tab[item-1, weight]
-    return tab[len(itemlist),totalweight]
+                values[item,weight] = values[item-1, weight]
+    return values[len(itemlist),totalweight]
 
 if __name__ == '__main__':
         
     items1 = [Item(2,3), Item(3,4), Item(4,8), Item(5,8), Item(9,10)]
     items2 = [Item(10,60), Item(20,100), Item(30,120)]
     print(knapsack_recursive(items1, 20))
-            
     print(knapsack_recursive(items2, 50))
     print(knapsack_dynamic(items1, 20))
-            
     print(knapsack_dynamic(items2, 50))
             
     
